ff7ec20f0cfbd391b7ae7f190f4bd5dc3a4be554
twyle <lyceokoth@gmail.com>
2023-04-21 15:54:15 +0300

    feat:creates the update workflow.

--- services/app/api/post/views.py
+++ services/app/api/post/views.py
@@ -1,5 +1,5 @@
 """This module contains routes for the app."""
-from flask import Blueprint, render_template, request, jsonify
+from flask import Blueprint, render_template, request, jsonify, url_for
 from flask_login import current_user, login_required
 
 from ..utils.http_status_codes import HTTP_200_OK, HTTP_201_CREATED
@@ -14,3 +14,42 @@ def create_post():
     form = request.form
     print(form)
     return jsonify({'success': 'created'}), HTTP_201_CREATED
+
+
+@post.route("/", methods=['GET'])
+@login_required
+def get_post():
+    """Render the home page."""
+    post_id = request.args.get('post_id')
+    print(post_id)
+    post = {
+        'post_id': post_id,
+        'user_photo': url_for('static', filename='img/profile-1.jpg'),
+        'user_name': 'Lyle Okoth',
+        'location': 'Nairobi',
+        'time': '10 MINUTES',
+        'post_text': 'New Post text from database.',
+        'post_photo': url_for('static', filename='img/feed-7.jpg')
+    }
+    return jsonify(post), HTTP_200_OK
+
+
+@post.route("/", methods=['DELETE'])
+@login_required
+def delete_post():
+    """Render the home page."""
+    post_id = request.args.get('post_id')
+    print(post_id)
+    return jsonify({'success': 'deleted'}), HTTP_200_OK
+
+
+@post.route("/", methods=['PUT'])
+@login_required
+def update_post():
+    """Render the home page."""
+    post_id = request.args.get('post_id')
+    file = request.files
+    form = request.form
+    print(file)
+    print(form)
+    return jsonify({'success': 'updated'}), HTTP_200_OK
--- services/app/api/static/js/index.js
+++ services/app/api/static/js/index.js
@@ -3,6 +3,12 @@ const createPostBtn = document.querySelector('.create-post-btn')
 const createPostFormDiv = document.querySelector('.create-post')
 const createPostForm = document.querySelector('.create-post-form')
 
+//EDIT 
+const editPostBtn = document.querySelector('.submit-edited-post')
+const editPostDiv = document.querySelector('.edit-post')
+const updatePostImage = document.querySelector('.post-image-upload')
+let image;
+let oldPost;
 
 //CREATE POST
 createPostBtn.addEventListener('click', () => {
@@ -31,4 +37,118 @@ createPostForm.addEventListener('submit', (e) => {
     )
 
     createPostFormDiv.style.display = 'none'
+})
+
+//DELETE POST
+document.addEventListener('click', (e) => {
+    if(e.target.classList.contains('fa-trash-can')){
+        const post = e.target.closest('.feed')
+        const postId = post.id
+        fetch(`http://localhost:5000/post?post_id=${postId}`,{
+            method: 'DELETE'
+        }).then(
+            response => response.json()
+        ).then(
+            response => {
+                console.log(JSON.stringify(response))
+            }
+        )
+        post.remove()
+    }
+})
+
+//EDIT POST
+document.addEventListener('click', (e) => {
+    if(e.target.classList.contains('fa-pen-to-square')){
+        oldPost = e.target.closest('.feed')
+        const postId = oldPost.id
+        editPostDiv.id = postId
+        const profilePhoto = editPostDiv.querySelector('.profile-photo-img')
+        const userName = editPostDiv.querySelector('h3')
+        const locationTime = editPostDiv.querySelector('small')
+        const postPhoto = editPostDiv.querySelector('.post-photo')
+        const textPhoto = editPostDiv.querySelector('.text-photo-img')
+        console.log(postPhoto)
+        fetch(`http://localhost:5000/post?post_id=${postId}`,{
+            method: 'GET'
+        }).then(
+            response => response.json()
+        ).then(
+            response => {
+                console.log(JSON.stringify(response))
+                profilePhoto.src = response['user_photo']
+                userName.textContent = response['user_name']
+                locationTime.textContent = `${response['location']}, ${response['time']} AGO`
+                postPhoto.src = response['post_photo']
+                textPhoto.src = response['user_photo']
+            }
+        )
+        editPostDiv.style.display = 'grid'
+    }
+})
+
+editPostDiv.addEventListener('click', (e) => {
+    if(e.target.classList.contains('edit-post')){
+        editPostDiv.style.display = 'none'
+    }
+})
+
+updatePostImage.addEventListener('click', () => {
+    const postPhoto = editPostDiv.querySelector('.post-photo')
+    const input = document.createElement('input');
+    input.type = 'file';
+
+    input.onchange = event => { 
+        const imageFiles = event.target.files
+        const imageFilesLength = imageFiles.length
+        if (imageFilesLength > 0){
+            image = imageFiles[0]
+            const imageSrc = URL.createObjectURL(imageFiles[0])
+            console.log(imageSrc)
+            postPhoto.src = imageSrc
+        }
+     }
+
+    input.click();
+})
+
+editPostBtn.addEventListener('click', (e) => {
+    const post = e.target.closest('.edit-post')
+    const postId = post.id
+    const postText = post.querySelector('.post-text')
+    const formData = new FormData()
+    if (postText.value){
+        formData.append('text', postText.value)
+    }
+    if (image){
+        formData.append("file", image)
+    }
+    fetch(`http://localhost:5000/post?post_id=${postId}`,{
+        method: 'PUT',
+        body: formData
+    }).then(
+        response => response.json()
+    ).then(
+        response => {
+            console.log(JSON.stringify(response))
+        }
+    )
+
+    const oldText = oldPost.querySelector('.post-text')
+    const oldImageDiv = oldPost.querySelector('.photo')
+    const oldImage = oldImageDiv.querySelector('img')
+
+    fetch(`http://localhost:5000/post?post_id=${postId}`,{
+            method: 'GET'
+        }).then(
+            response => response.json()
+        ).then(
+            response => {
+                console.log(JSON.stringify(response))
+                oldImage.src = response['post_photo']
+                oldText.textContent = response['post_text']
+            }
+        )
+
+    editPostDiv.style.display = 'none'
 })
